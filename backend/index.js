3b2657c6dc03f2197135464a6f7a9698a7b36aba
41682 <hikaru_hirahara@outlook.com>
2024-11-27 20:34:53 +0900

    index.js追加（'/api'パスにGET要求）

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+//'/api'パスにGET要求があった際に実行する処理
+app.get('/api', (req, res) => {
+  res.json({message: "Hello World"});
+});
 
 //3000ポートでlisten
 app.listen(port, () => {
